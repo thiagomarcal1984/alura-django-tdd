ca06df427c1f977ab74c8671c96bd3873ab04e22
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-12 20:11:01 -0300

    View teste.

--- animais/views.py
+++ animais/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = { 'caracteristicas': None }
+    return render(request, 'index.html', context)
