7c044775034d7f899e525898847cb9de5943d336
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-12 17:01:11 -0300

    Configurando o navegador.

--- setup/tests.py
+++ setup/tests.py
@@ -1,6 +1,11 @@
+from os import curdir, path, sep
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self) -> None:
-        pass
+        driver = "chromedriver.exe"
+        self.browser = webdriver.Chrome(f"{path.abspath(curdir)}{sep}{driver}")
+
+    def tearDown(self) -> None:
+        self.browser.quit()
