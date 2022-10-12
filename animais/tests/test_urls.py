01c6b09616ae5b7748c8a89bec236f9106dbfef8
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-12 18:08:52 -0300

    Testando URLs.

---
+++ animais/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+from animais.views import index
+
+class AnimaisURLSTestcase(TestCase):
+    def test_rota_url_utiliza_view_index(self):
+        """Teste se a home da aplicação utiliza a função index da view."""
+        root = reverse('/')
+        self.assertEqual(root.func, index)
