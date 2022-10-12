3cdcc6c6e50ca70d44066bcb6afc4ffd9c8b7186
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-12 17:32:22 -0300

    História do usuário.

--- setup/tests.py
+++ setup/tests.py
@@ -19,3 +19,20 @@ class AnimaisTestCase(LiveServerTestCase):
     def test_deu_ruim(self):
         """Teste de exemplo de erro."""
         self.fail("Teste falhou.")
+
+    def test_buscando_um_novo_animal(self):
+        """
+        Teste se um usuário encontra um animal pesquisando.
+        """
+        # Vini deseja encontrar um novo animal para adotar.
+
+        # Ele encontra o Busca Animal e decide usar o site, 
+        # porque ele vê no menu do site escrito Busca Animal.
+
+        # Ele vê um campo para pesquisar animais pelo nome.
+
+        # Ele pesquisa por Leão e clica no botão Pesquisar.
+
+        # O site exibe 4 características do animal pesquisado.
+
+        # Ele desiste de adotar um leão.
