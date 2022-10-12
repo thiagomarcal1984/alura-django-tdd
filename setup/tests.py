a06ea99d9d0675d7c58a4ba39e7d7bd4a3ec29aa
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-12 17:23:05 -0300

    URL do servidor e teste falha.

--- setup/tests.py
+++ setup/tests.py
@@ -9,3 +9,13 @@ class AnimaisTestCase(LiveServerTestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
+
+    # def test_para_verificar_se_a_janela_ok(self):
+    #     self.browser.get("http://www.google.com.br") # Busca URL externa.
+
+    def test_abre_janela_do_chrome(self):
+        self.browser.get(self.live_server_url) # Retorna URL do Django Server.
+
+    def test_deu_ruim(self):
+        """Teste de exemplo de erro."""
+        self.fail("Teste falhou.")
